e09bfabb40d9e7ac8f5a1dabbacb8aff1c90a8f9
man <maximillianaza@gmail.com>
2017-08-28 02:42:50 -0400

    Establish relationship between User & Post model

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/edit'
-
-  get 'users/create'
-
-  get 'users/update'
-
-  get 'users/destroy'
 
   devise_for :users
+  resources :users
   resources :posts
   root to: 'home#index'
 end
