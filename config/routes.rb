fa399551158ed8cdc3517b3ed27479cfb41500cb
man <maximillianaza@gmail.com>
2017-08-28 00:12:10 -0400

    Generate controller for User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/edit'
+
+  get 'users/create'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
   devise_for :users
   resources :posts
   root to: 'home#index'
